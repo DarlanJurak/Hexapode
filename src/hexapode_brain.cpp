3f3b272efa6851ebfc0124c9dcae63a3f8f94445
Darlan <darlan.jurak@gmail.com>
2017-11-06 21:11:19 -0200

    debugging

--- src/hexapode_brain.cpp
+++ src/hexapode_brain.cpp
@@ -349,27 +349,6 @@ void dynamicDebug(){//VideoCapture* cap){
          cout << "Cannot open the web cam" << endl;
     }
 
-    namedWindow("Control",CV_WINDOW_AUTOSIZE); //create a window called "Control"
-
-	int iLowH = 0;
-	int iHighH = 179;
-
-	int iLowS = 0; 
-	int iHighS = 255;
-
-	int iLowV = 0;
-	int iHighV = 255;
-
-	//Create trackbars in "Control" window
-	cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-	cvCreateTrackbar("HighH", "Control", &iHighH, 179);
-
-	cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-	cvCreateTrackbar("HighS", "Control", &iHighS, 255);
-
-	cvCreateTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
-	cvCreateTrackbar("HighV", "Control", &iHighV, 255);
-
     while (true)
     {
         Mat imgOriginal;
@@ -388,7 +367,12 @@ void dynamicDebug(){//VideoCapture* cap){
 	
 		Mat imgThresholded;
 
-		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+		inRange(
+				imgHSV, 
+				Scalar(	obstacles_mask[0].lowH,		obstacles_mask[0].lowS, 	obstacles_mask[0].lowV), 
+				Scalar(	obstacles_mask[0].highH,	obstacles_mask[0].highS,	obstacles_mask[0].highV), 
+				imgThresholded
+		);
       
 		//morphological opening (removes small objects from the foreground)
 		erode (imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
@@ -398,65 +382,12 @@ void dynamicDebug(){//VideoCapture* cap){
 		dilate(imgThresholded, 	imgThresholded,	getStructuringElement(MORPH_ELLIPSE, Size(5, 5))); 
 		erode (imgThresholded, 	imgThresholded,	getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-		Mat bitWisedImage, mask;
-		int mask_iLowH 	= mask_highBrightRubikCubeYellow_iLowH;
-		int mask_iHighH = mask_highBrightRubikCubeYellow_iHighH;
-		int mask_iLowS 	= mask_highBrightRubikCubeYellow_iLowS; 
-		int mask_iHighS = mask_highBrightRubikCubeYellow_iHighS;
-		int mask_iLowV 	= mask_highBrightRubikCubeYellow_iLowV;
-		int mask_iHighV = mask_highBrightRubikCubeYellow_iHighV;
-
-		// int mask_iLowH 	= iLowH;
-		// int mask_iHighH = iHighH;
-		// int mask_iLowS 	= iLowS; 
-		// int mask_iHighS = iHighS;
-		// int mask_iLowV 	= iLowV;
-		// int mask_iHighV = iHighV;
-
-		//Creates mask. Bounds defines chased color.
-		inRange(imgHSV, Scalar(mask_iLowH, mask_iLowS, mask_iLowV), Scalar(mask_iHighH, mask_iHighS, mask_iHighV), mask);
-		bitwise_and(imgOriginal, imgOriginal, bitWisedImage, mask);
-
 		imshow("Thresholded Image", imgThresholded); 	//show the thresholded image
-		imshow("Original", 			imgOriginal); 		//show the original image
-		imshow("HSV Image", 		imgHSV); 			//show the HSV image
-		imshow("Bitwised Image", 	bitWisedImage); 	//show the Filtered image
-
-		Vec3d pix;
-        double hue;
-		double sat;
-		double val;
-
-		for( int i = 0; i < bitWisedImage.rows; i++){
-			for( int j = 0; j < bitWisedImage.cols; j++){
-
-				pix = bitWisedImage.at<typeof(pix)>(i,j);
-				hue = pix.val[0];
-				sat = pix.val[1];
-				val = pix.val[2];
-
-				// if( (hue > 1) && (sat > 1) && (val > 1)){
-
-				// 	cout << "Hue: " << hue << ". Sat: " << sat << ". Val: " << val << "." << endl;
-				// }
-			}				
-		}
 	    
 	   
     	if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
       	{
             cout << "esc key is pressed by user" << endl;
-
-			// cout << "\nCanais: " << bitWisedImage.channels() << "\nLinhas: " << bitWisedImage.rows << "\nColunas: " << bitWisedImage.cols << endl;
-
-            // Show last pixel info
-			pix	= bitWisedImage.at<typeof(pix)>(bitWisedImage.rows -1, bitWisedImage.cols -1);
-			hue = pix.val[0];
-			sat = pix.val[1];
-			val = pix.val[2];
-
-			cout << "\nHue: " << hue << "\nSat: " << sat << "\nVal: " << val << endl;
-
             break; 
        	}
 
